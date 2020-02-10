45ba885288bb970d3d2cd0ca041d9e9e1600ee4e
Berdil İlyada <ilyadaberdil@gmail.com>
2020-02-11 00:57:59 +0300

    Dependency

---
+++ ComponentBaseDependencyInjection/Supporting Files/Dependency.swift
@@ -0,0 +1,16 @@
+//
+//  Dependencies.swift
+//  ComponentBaseDependencyInjection
+//
+//  Created by Berdil İlyada Karacam on 8.02.2020.
+//  Copyright © 2020 Berdil İlyada Karacam. All rights reserved.
+//
+
+import Foundation
+
+//MARK: Sample Dependencies
+struct WebService { }
+struct FirstScreenParameter { }
+struct SecondScreenParameter { }
+struct SpecialFormatter { }
+
